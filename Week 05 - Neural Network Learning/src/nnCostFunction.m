f7e901460486e4a4aa55f5ba5dbb2ac49871ac77
Radu Tanasa <radu@Radus-MacBook-Pro.local>
2014-11-27 22:36:39 +0200

    Finished week 5

--- Week 05 - Neural Network Learning/src/nnCostFunction.m
+++ Week 05 - Neural Network Learning/src/nnCostFunction.m
@@ -70,10 +70,11 @@ for i=1:m
   Y(i, :)= I(y(i), :);
 end
 
-X = [ones(size(X, 1), 1) X];
-A2 = sigmoid(X*Theta1');
-A2 = [ones(size(A2, 1), 1) A2];
-H = A3 = sigmoid(A2*Theta2');
+A1 = [ones(size(X, 1), 1) X];
+Z2 = A1*Theta1';
+A2 = [ones(size(Z2, 1), 1) sigmoid(Z2)];
+Z3 = A2*Theta2';
+H = A3 = sigmoid(Z3);
 
 J = (1/m)*sum(sum((-Y).*log(H) - (1-Y).*log(1-H), 2));
 
@@ -83,15 +84,17 @@ reg = (lambda/(2*m))*(sum(sum(Theta1(:, 2:end).^2, 2)) + sum(sum(Theta2(:,2:end)
 J = J + reg;
 
 
+Sigma3 = A3 - Y;
+% we do not include sigma zero (index 1 in octave)
+Sigma2 = (Sigma3*Theta2 .* sigmoidGradient([ones(size(Z2, 1), 1) Z2]))(:, 2:end);
 
 
+Delta_1 = Sigma2'*A1;
+Delta_2 = Sigma3'*A2;
 
 
-
-
-
-
-
+Theta1_grad = Delta_1./m + (lambda/m)*[zeros(size(Theta1,1), 1) Theta1(:, 2:end)];
+Theta2_grad = Delta_2./m + (lambda/m)*[zeros(size(Theta2,1), 1) Theta2(:, 2:end)];
 
 
 
